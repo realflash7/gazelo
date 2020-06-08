8b82e36d2eba49548ed961d2027b78346cac1163
Piyush SIngh <piyush.singh@salesforce.com>
2020-06-08 10:53:10 +0530

    superuser

--- outset/admin.py
+++ outset/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Video, View, Like, Comment, Following
+
+admin.site.register(User)
+admin.site.register(Video)
+admin.site.register(View)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Following)
+
