59176a25d6a54229e98eeefaf1d92219443ee05a
Piyush SIngh <piyush.singh@salesforce.com>
2020-06-08 20:08:51 +0530

    added description,publish date, comments  in video

--- outset/models.py
+++ outset/models.py
@@ -11,28 +11,47 @@ class User(models.Model):
     followers_count = models.IntegerField()
     following_count = models.IntegerField()
 
+    def __str__(self):
+        return "{0}".format(
+            self.user_name)
+
 class Video(models.Model):
     name = models.CharField(max_length=400, blank=False)
     user = models.ForeignKey(User, related_name="added_by", on_delete=models.CASCADE)
+    description = models.TextField(max_length=40000, null=True, blank=True, default="")
     data_source = models.CharField(max_length=400, blank=True, default="NONE" , null=False)
     created_date = models.DateTimeField(auto_now_add=True)
     last_modified_date = models.DateTimeField(auto_now_add=True)
+    publish_date = models.DateTimeField(null=True, blank=True)
     access_level = models.IntegerField() #maybe - 0=private, 1=public, 2=masked
     url_key = models.CharField(max_length=10, blank=False)
     size = models.IntegerField()
     views_count = models.IntegerField()
     likes_count = models.IntegerField()
     comments_count = models.IntegerField()
+    allow_comments = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{0}".format(
+            self.name)
 
 class View(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name="viewed_by", on_delete=models.CASCADE)
     last_view_date = models.DateTimeField()
 
+    def __str__(self):
+        return "{0} viewed {1}".format(
+            self.user, self.video)
+
 class Like(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name="liked_by", on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{0} liked {1}".format(
+            self.user, self.video)
 
 class Comment(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
@@ -43,8 +62,16 @@ class Comment(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     last_modified_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return "{1} commented \"{1}\" on {2}".format(
+            self.user, self.text, self.video)
+
 class Following(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    following = models.ForeignKey(User, related_name="follower", on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, related_name="follower", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0} follows {1}".format(
+            self.follower, self.user)
 
 
