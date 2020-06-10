27fd1aa00400f8761f51a275d08dd281789a91b7
Piyush SIngh <piyush.singh@salesforce.com>
2020-06-10 21:05:56 +0530

    C4

--- video_stream/urls.py
+++ video_stream/urls.py
@@ -1,9 +1,14 @@
-from django.conf.urls import url
-
 from django.urls import path
 
 from .views import home
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', home, name='user_home'),
+    path('login/',
+         LoginView.as_view(template_name="video_stream/login_form.html"),
+         name="video_stream_login"),
+    path('logout/',
+         LogoutView.as_view(),
+         name="video_stream_logout")
 ]
